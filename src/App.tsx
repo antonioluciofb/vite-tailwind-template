fccb534b447507ee2233a4c5d2a7d118e1da5a3e
antonioluciofb <antonioluciofb@gmail.com>
2023-12-16 17:16:49 -0300

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+interface IApp {}
+
+// eslint-disable-next-line no-empty-pattern
+const App: React.FC<IApp> = ({}) => {
+  return <div className="w-screen h-screen bg-gray-900">Hello World</div>;
+};
+
+export default App;
